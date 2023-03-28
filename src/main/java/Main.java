049f48e835d80a83b0c721b204fc7a6347ea9515
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-03-28 13:48:21 +0100

    print_Buzz_when_number_contain_5_but_not_3_and_not_is_divisible_by_5_and_3 verde

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,26 @@ public class Main {
 
     public static String checkNumber(int number) {
 
-        boolean is_dividible_by_3 = number % 3 == 0;
+        boolean is_divisible_by_3 = number % 3 == 0;
+        boolean is_divisible_by_5 = number % 5 == 0;
         boolean contain_3= String.valueOf(number).contains("3");
+        boolean contain_5= String.valueOf(number).contains("5");
+
         String message="";
 
-        if (is_dividible_by_3) {
-             message+="Fizz";
+        if (is_divisible_by_3) {
+            message+="Fizz";
+        }
+        if (is_divisible_by_5){
+            message+="Buzz";
         }
         if (contain_3){
-             message+="Fizz";
+            message+="Fizz";
         }
-        if(!is_dividible_by_3 &&!contain_3){
+        if (contain_5){
+            message+="Buzz";
+        }
+        if(!is_divisible_by_3 && !contain_3 && !is_divisible_by_5 && !contain_5){
             message=String.valueOf(number);
         }
         return message;
--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -24,37 +24,44 @@ public class FizzBuzzShould {
 
 
     @Test
-    void print_same_number_when_it_is_not_divisible_by_3_and_5_and_no_contain_3_or_5(){
-        int number=2;
-        String result= Main.checkNumber(number);
-        assertEquals(String.valueOf(number),result);
+    void print_same_number_when_it_is_not_divisible_by_3_and_5_and_no_contain_3_or_5() {
+        int number = 2;
+        String result = Main.checkNumber(number);
+        assertEquals(String.valueOf(number), result);
     }
 
     @Test
-    void print_Fizz_if_number_is_divisible_by_3(){
-        int number=3;
-        String result= Main.checkNumber(number);
-        assertEquals("Fizz",result);
+    void print_Fizz_if_number_is_divisible_by_3_but_no_by_5_and_not_contain_3_or_5() {
+        int number = 12;
+        String result = Main.checkNumber(number);
+        assertEquals("Fizz", result);
     }
 
     @Test
-    void print_Fizz_if_number_is_divisible_by_3_and_contain_3(){
-        int number=30;
-        String result= Main.checkNumber(number);
-        assertEquals("Fizz",result);
+    void print_Fizz_if_number_is_no_divisible_by_3_or_5_and_contain_3_but_not_5() {
+        int number = 13;
+        String result = Main.checkNumber(number);
+        assertEquals("Fizz", result);
     }
 
     @Test
-    void print_FizzFizz_when_number_is_dividibleby_3_but_not_by_5_and_contain_3_but_no_5() {
+    void print_FizzFizz_when_number_is_dividible_by_3_but_not_by_5_and_contain_3_but_not_5() {
         int number = 3;
         String result = Main.checkNumber(number);
         assertEquals("FizzFizz", result);
     }
-    //any number divisible by 3 but no by 5 and contain 5 but no 3-> FizzBuzz (51)
+
     @Test
     void print_FizzBuzz_when_number_is_dividible_by_3_but_not_by_5_and_contain_5_but_no_3() {
         int number = 51;
         String result = Main.checkNumber(number);
         assertEquals("FizzBuzz", result);
+    }
+    //any number contain 5 but no 3 and is not divisible by 3 or 5 ->Buzz (56)
+    @Test
+    void print_Buzz_when_number_contain_5_but_not_3_and_not_is_divisible_by_5_and_3() {
+        int number = 56;
+        String result = Main.checkNumber(number);
+        assertEquals("Buzz", result);
     }
 }
